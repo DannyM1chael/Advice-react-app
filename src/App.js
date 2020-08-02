1b88682e377ae2562fd376761235372c599225b3
DannyM1chael <coldsoulrusirk@gmail.com>
2020-08-02 08:55:08 +0300

    view fix, remove useless code

--- src/App.js
+++ src/App.js
@@ -3,29 +3,29 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-
-  const [advice, setAdvice] = useState('');
+  const [advice, setAdvice] = useState(null);
 
   useEffect(() => {
     FetchAdvice();
   }, []);
 
   const FetchAdvice = () => {
-    axios.get('	https://api.adviceslip.com/advice')
-        .then((response) => {
-          const { advice } = response.data.slip;
-          setAdvice(advice);
-        })
-        .catch((error) => {
-          console.log(error)
-        });
-  }
+    axios
+      .get('	https://api.adviceslip.com/advice')
+      .then((response) => {
+        const { advice } = response.data.slip;
+        setAdvice(advice);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div className="app">
       <div className="card">
-        <h1 className="heading">{ advice }</h1>
-        <button className="button" onClick={ FetchAdvice }>
+        <h1 className="heading">{advice}</h1>
+        <button className="button" onClick={FetchAdvice}>
           <span>Give me advice</span>
         </button>
       </div>
